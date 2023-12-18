0c991397fec6dae182b9066833f1037a8dbafcf1
PaoloTarcisio <paoloc50@gmail.com>
2023-12-18 16:32:43 +0100

    milestone 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const WayneBarnett = {nome: 'Wayne Barnett', ruolo: 'Founder & CEO', immagine: 'wayne-barnett-founder-ceo.jpg'}
+const AngelaCaroll = {nome: 'Angela Caroll', ruolo: 'Chief Editor', immagine: 'angela-caroll-chief-editor.jpg'}
 const WalterGordon = {nome: 'Walter Gordon', ruolo: 'Office Manager', immagine: 'walter-gordon-office-manager.jpg'}
 const AngelaLopez = {nome: 'Angela Lopez', ruolo: 'Social Media Manager', immagine: 'angela-lopez-social-media-manager.jpg'}
 const ScottEstrada = {nome: 'Scott Estrada', ruolo: 'Developer', immagine: 'scott-estrada-developer.jpg'}
@@ -13,4 +14,10 @@ const team = {
     BarbaraRamos,
 };
 
-
+console.log(team);
+console.log(WayneBarnett);
+console.log(AngelaCaroll);
+console.log(WalterGordon);
+console.log(AngelaLopez);
+console.log(ScottEstrada);
+console.log(BarbaraRamos);
