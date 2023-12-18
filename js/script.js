e09e6dfa44cc7b0d2e7c03ccac05971870efe9e3
PaoloTarcisio <paoloc50@gmail.com>
2023-12-18 16:28:34 +0100

    milestone 0

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const WayneBarnett = {nome: 'Wayne Barnett', ruolo: 'Founder & CEO', immagine: 'wayne-barnett-founder-ceo.jpg'}
+const WalterGordon = {nome: 'Walter Gordon', ruolo: 'Office Manager', immagine: 'walter-gordon-office-manager.jpg'}
+const AngelaLopez = {nome: 'Angela Lopez', ruolo: 'Social Media Manager', immagine: 'angela-lopez-social-media-manager.jpg'}
+const ScottEstrada = {nome: 'Scott Estrada', ruolo: 'Developer', immagine: 'scott-estrada-developer.jpg'}
+const BarbaraRamos = {nome: 'Barbara Ramos', ruolo: 'Graphic Designer', immagine: 'barbara-ramos-graphic-designer.jpg'}
+
+
+const team = {
+    WayneBarnett,
+    WalterGordon,
+    AngelaLopez,
+    ScottEstrada,
+    BarbaraRamos,
+};
+
+
